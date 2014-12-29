ab9f76aaac015e5b1e499ff3ebfc34c00d10f65e
dookim <doo871128@gmail.com>
2014-12-30 07:51:20 +0900

    day is changed

--- deleter.py
+++ deleter.py
@@ -8,12 +8,10 @@ import os
 last_index_file ="last_index.txt"
 from_index = 0;
 if os.path.exists(last_index_file):
-    pid_file=open(last_index_file, "rw")
+    pid_file=open(last_index_file, "r+")
     from_index=int(pid_file.readline().strip());
-else :
-    pid_file=open(last_index_file, "w+")
-
-start_date = datetime.date.today() + datetime.timedelta(-15)
+   
+start_date = datetime.date.today() + datetime.timedelta(-5)
 start_date =  start_date.strftime("%Y:%m:%d %H-%M-%S")
 
 query = "SELECT * FROM instances WHERE datetime <= '{datetime}' ORDER BY idinstance desc limit 1;".format(datetime = start_date)
@@ -23,13 +21,18 @@ print query;
 before_fetched = session.execute(query);
 data = before_fetched.fetchone();
 to_index = data[0];
+
+print "to index : " + str(to_index)
 path = "../poolfromworker/logpool/"
-
 for i in range(from_index, to_index):
-    file_path = path + i + ".txt"
-    print file_path
-    os.remove(file_path)
+    try :
+        file_path = path + str(i) + ".txt"
+        print file_path
+        os.remove(file_path)
+    except Exception as e:
+        print e
+        continue;
 
-
+pid_file.seek(0);
 pid_file.write(str(to_index))
 pid_file.close()
